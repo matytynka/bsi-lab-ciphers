dc25186189d099a8d3f82c4f86e6f08d9627dedf
Martyna <matytynka@o2.pl>
2021-01-04 17:37:38 +0100

    DSAExample and AESExample documentation updated

--- ciphers/AESExample.py
+++ ciphers/AESExample.py
@@ -1,14 +1,35 @@
 from Crypto.Cipher import AES
 
 class AESExample:
+    """
+    A class to represent an RSAExample
 
+    METHODS
+    -------
+    encrypt(msg):
+        Signs the msg and returns it.
+
+    sign_and_verify(msg):
+        Signs and verifies the msg and returns it.
+    """
     def __init__(self):
+        """
+        Constructs the RSAExample object.
+        """
         self.key = b'FSMF73R873YM187R'
         self.signer = AES.new(self.key, AES.MODE_EAX)
         self.verifier = AES.new(self.key, AES.MODE_EAX, nonce=self.signer.nonce)
 
 
     def sign_and_verify(self, msg):
+        """
+        Verifies and signs the msg.
+
+        Parameters
+        ----------
+        msg : str
+            Message to be verified
+        """
         ciphertext, tag = self.signer.encrypt_and_digest(msg.encode('utf-8'))
         plaintext = self.verifier.decrypt(ciphertext)
         try:
